725f99974cb316d1936a19afb86ef6b9c9900395
Joe <joe.lawson.programmer@gmail.com>
2015-06-21 11:16:02 -0500

    * more changes

--- Build-Scripts/decrypt.sh
+++ Build-Scripts/decrypt.sh
@@ -9,7 +9,7 @@ DLL_DIR="Source/A2B/Source-DLLs"
 
 pushd $DLL_DIR
 
-openssl aes-256-cbc -K $encrypted_d8f806c9345c_key -iv $encrypted_d8f806c9345c_iv -in dlls.7z.enc -out dlls.7z -d
+openssl aes-256-cbc -K $1 -iv $2 -in dlls.7z.enc -out dlls.7z -d
 
 # Extract the archive
 7z x dlls.7z
